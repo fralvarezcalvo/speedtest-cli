4d435e85a3d45002829c8263420b5ad931898275
fralvarezcalvo <fralvarezcalvo@gmail.com>
2021-04-24 17:46:40 +0200

    WIP

---
+++ .github/workflows/ci.generate_json_matrix.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This file just generates a github-actions matrix type to build the images with the corresponding binary. 
+
+JSON_MATRIX="{ \"include\": [] }"
+while IFS="=", read -r ARCH BIN
+do
+    JSON_MATRIX=$(echo "$JSON_MATRIX" | jq ".include += [
+        {\"arch\": \"$ARCH\",\"bin\": \"$BIN\"} ]")
+done < MAP_ARCH_TRANSLATOR
+
+echo "::set-output name=matrix::$JSON_MATRIX"

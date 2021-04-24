977d997e5a8feb07cedb52d8b56f9f0efe2751ab
fralvarezcalvo <fralvarezcalvo@gmail.com>
2021-04-24 18:53:11 +0200

    WIP

--- .github/workflows/ci.generate_json_matrix.sh
+++ .github/workflows/ci.generate_json_matrix.sh
@@ -5,8 +5,10 @@
 JSON_MATRIX="{ \"include\": [] }"
 while IFS="=", read -r ARCH ARCH_BIN
 do
+    # Replaces the arch / by _ to use as docker labels
+    DOCKER_ARCH_LABEL=$(echo "$ARCH" | sed "s/\//_/")
     JSON_MATRIX=$(echo "$JSON_MATRIX" | jq ".include += [
-        {\"arch\": \"$ARCH\",\"bin_arch\": \"$ARCH_BIN\"} ]")
+        {\"arch\": \"$ARCH\",\"bin_arch\": \"$ARCH_BIN\", \"docker_arch_label\": \"$DOCKER_ARCH_LABEL\"} ]")
 done < MAP_ARCH_TRANSLATOR
 
 JSON_MATRIX=$( echo "$JSON_MATRIX" | jq -c . )
